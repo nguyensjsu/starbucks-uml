0217b00b1653f5968c537bcd6989af65da4a0921
saiprithipa <sai.prithiparammohan@gmail.com>
2019-10-13 20:07:43 -0700

    Using shadow jar to avoid dependency on m2 repository

--- junit2uml-runner-gradle/test2uml.sh
+++ junit2uml-runner-gradle/test2uml.sh
@@ -1,2 +1,3 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-java -jar $parent_path/build/libs/junit2uml-runner-gradle-1.0-SNAPSHOT-all.jar com.saip.Main $1 $2 $3 $4
+#java -jar $parent_path/build/libs/junit2uml-runner-gradle-1.0-SNAPSHOT-all.jar #com.saip.Main $1 $2 $3 $4
+java -jar /Users/nishant/git/starbucks-git/starbucks-uml/libraries/junit2uml-runner.jar com.saip.Main $1 $2 $3 $4
